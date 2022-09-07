82fd656b95632dc4deb496003156d759cf124f49
geisPawaheetimPower <113019431+geisPawaheetimPower@users.noreply.github.com>
2022-09-07 17:26:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.c0rjyw0pzejy.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "chenkokfong-employee"
 customregion = "us-east-1"
